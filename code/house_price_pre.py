37bb6a7ebbe55fa09dfcb6dc8141e4d46f940f7d
jianxia318 <40734048+jianxia318@users.noreply.github.com>
2018-07-07 22:55:08 +0800

    aa

--- code/house_price_pre.py
+++ code/house_price_pre.py
@@ -154,3 +154,15 @@ all_data = pd.get_dummies(all_data)
 
 train = all_data[:ntrain]
 test = all_data[ntrain:]
+
+#modeling
+from sklearn.linear_model import ElasticNet, Lasso,  BayesianRidge, LassoLarsIC
+from sklearn.ensemble import RandomForestRegressor,  GradientBoostingRegressor
+from sklearn.kernel_ridge import KernelRidge
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import RobustScaler
+from sklearn.base import BaseEstimator, TransformerMixin, RegressorMixin, clone
+from sklearn.model_selection import KFold, cross_val_score, train_test_split
+from sklearn.metrics import mean_squared_error
+import xgboost as xgb
+import lightgbm as lgb
